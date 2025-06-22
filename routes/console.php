c6e4a6089b895971d06ababf0bbf83a7862407bf
zahrandiaz <zahrandiaz99@gmail.com>
2025-06-22 19:26:51 +0700

    fix(schedule): Pindahkan definisi schedule ke routes/console.php

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,14 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule; // <-- Pastikan use statement ini ada
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+
+// [MODIFIKASI] Tambahkan penjadwalan untuk queue worker di sini
+Schedule::command('queue:work --stop-when-empty')
+         ->everyMinute()
+         ->withoutOverlapping();
